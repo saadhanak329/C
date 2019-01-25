a4ec0a12606b88f97280cc6a892034b67d310ca2
Saadhana Reddy k <saadhanak329@gmail.com>
2019-01-25 10:37:05 +0530

    Added Notes and HelloWorld Program

---
+++ Problems/HelloWorld.c
@@ -0,0 +1,13 @@
+/*
+Hello World!
+
+Purpose: This program prints "Hello World!" on output screen
+
+Author: Saadhana Reddy K
+*/
+#include <stdio.h>
+
+void main()
+{
+    printf("\nHello World!\n");
+}
